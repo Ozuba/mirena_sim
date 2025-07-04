993d331b0b8dd891e5874ff8623249106bbf2106
colmenasio <a.colmenero.fernandez@gmail.com>
2025-07-04 13:15:00 +0200

    [F] Implemented ros bridge

---
+++ src/ros_bridge/mirena_ros_bridge.cpp
@@ -0,0 +1,16 @@
+#include "mirena_ros_bridge.hpp"
+
+using namespace mirena;
+using namespace godot;
+
+MirenaRosBridge::MirenaRosBridge() : _ros_node(rclcpp::Node("mirena_ros_bridge")),
+                                     _debugFullTrackPub(this->_ros_node.create_publisher<mirena_common::msg::BezierCurve>(DEBUG_FULL_TRACK_TOPIC, 10)),
+                                     _debugImmediateTrackPub(this->_ros_node.create_publisher<mirena_common::msg::BezierCurve>(DEBUG_IMMEDIATE_TRACK_TOPIC, 10))
+{
+}
+
+void MirenaRosBridge::_bind_methods()
+{
+    ClassDB::bind_method(D_METHOD("publish_full_track_curve", "curve"), &MirenaRosBridge::_publish_full_track_curve);
+    ClassDB::bind_method(D_METHOD("publish_immediate_track_curve", "curve"), &MirenaRosBridge::_publish_immediate_track_curve);
+}
