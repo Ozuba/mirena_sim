d6d479bf10bc8fafbae8975817eb499f5a2f6585
colmenasio <a.colmenero.fernandez@gmail.com>
2025-07-04 23:32:19 +0200

    [F] Renamed active ros node

---
+++ src/ros/active_ros_node.cpp
@@ -0,0 +1,46 @@
+#include "active_ros_node.hpp"
+
+mirena::ActiveRosNode::ActiveRosNode(const std::string &node_name, const rclcpp::NodeOptions &options = rclcpp::NodeOptions()) : ActiveRosNode(rclcpp::Node::make_shared(node_name, options))
+{
+}
+
+mirena::ActiveRosNode::ActiveRosNode(const rclcpp::Node::SharedPtr node) : _node(node)
+{
+    _spin_thread = std::thread([&](){this->spin();});
+}
+
+mirena::ActiveRosNode::~ActiveRosNode()
+{
+    _shutdown_flag.store(true);
+    _spin_thread.join();
+}
+
+void mirena::ActiveRosNode::set_paused(bool value)
+{
+    _paused_flag.store(value);
+}
+
+bool mirena::ActiveRosNode::get_paused()
+{
+    return _paused_flag.load();
+}
+
+void mirena::ActiveRosNode::set_spin_period_ms(int64_t value)
+{
+    _spin_period_ms.store(std::chrono::milliseconds(value));
+}
+
+int64_t mirena::ActiveRosNode::get_spin_period_ms()
+{
+    return _spin_period_ms.load().count();
+}
+
+void mirena::ActiveRosNode::spin()
+{
+    while (_shutdown_flag.load() != true){
+        if (_paused_flag.load() != true){
+            rclcpp::spin_some(_node);
+        }
+        std::this_thread::sleep_for(_spin_period_ms.load());
+    }
+}
