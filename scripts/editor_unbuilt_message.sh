612ee653696cedec06528d69242808577ca3420c
colmenasio <a.colmenero.fernandez@gmail.com>
2025-09-08 12:50:53 +0200

    [F] Now editor unbuild warning feels bonita

--- scripts/editor_unbuilt_message.sh
+++ scripts/editor_unbuilt_message.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-echo "The editor-related source code is not built by default."
-echo "If you just want to run the sim without editing the sim in godot, run 'ros2 run mirena_sim sim' instead"
-echo "To build the editor, run:"
-echo ""
-echo "colcon build --cmake-args -DGD_EDITOR=ON"
-echo ""
-echo "This will download/update the godot executable and build the GDExtension library"
-echo "After that, feel free to source ros and run 'ros2 run mirena_sim godot'"
+ORANGE='\033[0;33m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
 
+echo -e "${ORANGE}The editor-related source code is not built by default.${NC}"
+echo -e "${ORANGE}If you just want to run the sim without editing the sim in godot, run 'ros2 run mirena_sim sim' instead${NC}"
+echo -e "${ORANGE}To build the editor, run:${NC}"
+echo -e "${ORANGE}${NC}"
+echo -e "	${GREEN}colcon build --cmake-args -DGD_EDITOR=ON${NC}"
+echo -e "${ORANGE}${NC}"
+echo -e "${ORANGE}This will download/update the godot executable and build the GDExtension library${NC}"
+echo -e "${ORANGE}After that, feel free to source ros and run 'ros2 run mirena_sim godot'${NC}"
